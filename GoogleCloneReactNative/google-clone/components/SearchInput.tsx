64319885d2149f763892ea8ef8105c7b5809cbb5
roxanap33 <roxana.ciocian@principal33.com>
2024-04-05 19:52:34 +0300

    Add microphone icon on the search input

--- GoogleCloneReactNative/google-clone/components/SearchInput.tsx
+++ GoogleCloneReactNative/google-clone/components/SearchInput.tsx
@@ -1,9 +1,19 @@
-import {StyleSheet, TextInput, View} from 'react-native';
+import {Image, StyleSheet, TextInput, View} from 'react-native';
 
 export default function SearchInput() {
   return (
     <View style={styles.rootContainer}>
-      <TextInput style={styles.input} placeholder="Search here" />
+      <View style={styles.inputContainer}>
+        <Image
+          source={require('../assets/icons/search.png')}
+          style={styles.searchIcon}
+        />
+        <TextInput style={styles.input} placeholder="Search here" />
+        <Image
+          source={require('../assets/icons/mic.png')}
+          style={styles.micIcon}
+        />
+      </View>
     </View>
   );
 }
@@ -14,12 +24,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    minWidth: '100%',
-    height: 40,
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderWidth: 1,
     borderColor: '#cccccc',
     borderRadius: 25,
     paddingHorizontal: 10,
+    maxWidth: '100%',
+    height: 40,
+  },
+  searchIcon: {
+    width: 20,
+    height: 20,
+    left: 0,
+  },
+  micIcon: {
+    width: 20,
+    height: 20,
+    position: 'absolute',
+    right: 10,
+  },
+  input: {
+    minWidth: '90%',
+    paddingLeft: 10,
+    paddingRight: 50,
   },
 });
